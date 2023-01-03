180a19433daf25150709939ff760092ef7bc217f
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-03 20:29:15 +0000

    generate greetings

---
+++ spec/requests/greetings_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Greetings", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/greetings/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
