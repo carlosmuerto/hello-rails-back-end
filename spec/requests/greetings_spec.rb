4413ba9a978fe9230c2824b75a08596c30b51118
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-03 20:47:39 +0000

    rubocop -A

--- spec/requests/greetings_spec.rb
+++ spec/requests/greetings_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Greetings", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/greetings/index"
+RSpec.describe 'Greetings', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/greetings/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
